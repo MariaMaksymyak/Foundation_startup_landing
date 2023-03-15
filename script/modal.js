8238ec4085e4b1e2c33fdbc27625a93584d0e231
MariaMaksymyak <114564412+MariaMaksymyak@users.noreply.github.com>
2023-03-15 10:23:07 +0200

    Add files via upload

--- script/modal.js
+++ script/modal.js
@@ -11,3 +11,36 @@ $('.close').on('click', function(){
 $('#open-modal').on('click', function(){
     $('.modal-overlay').fadeIn()
 })
+$('#open-modal1').on('click', function(){
+    $('.modal-overlay').fadeIn()
+})
+$('#open-modal2').on('click', function(){
+    $('.modal-overlay').fadeIn()
+})
+
+
+
+
+$('.modal-overlay_get_start').on('click', function(e){
+    if($(e.target).closest('.modal_start').length == 0){
+       $(this).fadeOut();
+    }
+});
+
+$('.close').on('click', function(){
+    $(this).parents('.modal-overlay_get_start').fadeOut()
+})
+
+$('#open-modal_start').on('click', function(){
+    $('.modal-overlay_get_start').fadeIn()
+})
+$('#open-modal_start2').on('click', function(){
+    $('.modal-overlay_get_start').fadeIn()
+})
+$('#open-modal_start3').on('click', function(){
+    $('.modal-overlay_get_start').fadeIn()
+})
+$('#open-modal_start4').on('click', function(){
+    $('.modal-overlay_get_start').fadeIn()
+})
+    
