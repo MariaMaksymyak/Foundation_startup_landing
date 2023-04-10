32e910493ae940f00fbcbbaa9504dc43b56948aa
MariaMaksymyak <114564412+MariaMaksymyak@users.noreply.github.com>
2023-04-10 11:50:19 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,31 +1,10 @@
-var slideIndex =1;
-showSlides(slideIndex);
+$('.features_item > div').on('click', function(){
+    $(this).next('.features_text').slideToggle(300);
+})
 
-function plusSlides(n){
-    showSlides(slideIndex += n);
-}
 
-function currentSlides(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n){
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-
-    if (n >slides.length) {
-        slideIndex =1
-    }
-    if (n < 1){
-        slideIndex=slides.length
-    }
-    for (i=0; i < slides.length; i++){
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace("active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className+= " active";
-}
+$('.about-items > div').on('click', function(){
+    $(this).children('.about-icon').toggleClass('about-rotate-icon');
+    $(this).parent('.about-items').toggleClass('change-color');
+    $(this).next('.about-text').slideToggle(300);
+})
