b4b6ec5668f63b897af3839d7e85c3b28bafc4dd
MariaMaksymyak <114564412+MariaMaksymyak@users.noreply.github.com>
2023-04-10 11:48:22 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,10 +1,31 @@
-$('.features_item > div').on('click', function(){
-    $(this).next('.features_text').slideToggle(300);
-})
+var slideIndex =1;
+showSlides(slideIndex);
 
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
 
-$('.about-items > div').on('click', function(){
-    $(this).children('.about-icon').toggleClass('about-rotate-icon');
-    $(this).parent('.about-items').toggleClass('change-color');
-    $(this).next('.about-text').slideToggle(300);
-})
+function currentSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n >slides.length) {
+        slideIndex =1
+    }
+    if (n < 1){
+        slideIndex=slides.length
+    }
+    for (i=0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className+= " active";
+}
